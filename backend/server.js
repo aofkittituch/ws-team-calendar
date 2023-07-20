d8d5b1116e1e75fd3315abe97a68a8e1012d69fc
DESKTOP-E9KBGN3\AOF <off_17212@hotmail.com>
2023-07-20 19:13:41 +0700

    create backend

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mysql2 = require("mysql2");
+
+const conn = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "ws_team",
+});
+
+if (conn) {
+  console.log("Connecting DB successfully");
+} else {
+  console.log("Failed to connect DB");
+}
+
+app.get("/all_cal", async (req, res) => {
+  let sql =
+    "SELECT tc.cal_id, tc.cal_name, tc.cal_image, tct.type_name as cal_type, tc.cal_price, tc.cal_is_active FROM tbl_calendar as tc LEFT JOIN tbl_calendar_type as tct ON tct.type_id = tc.cal_id";
+
+  await conn.execute(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+      return;
+    }
+    res.status(200).json({
+      message: true,
+      data: result,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
