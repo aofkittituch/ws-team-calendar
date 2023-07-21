8c06e32b477044ac93da91669bbf680929f445d4
DESKTOP-E9KBGN3\AOF <off_17212@hotmail.com>
2023-07-21 18:35:14 +0700

    fix backend & add function get type calendar get all calendar

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const mysql2 = require("mysql2");
@@ -16,9 +18,30 @@ if (conn) {
   console.log("Failed to connect DB");
 }
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/all_cal", async (req, res) => {
   let sql =
     "SELECT tc.cal_id, tc.cal_name, tc.cal_image, tct.type_name as cal_type, tc.cal_price, tc.cal_is_active FROM tbl_calendar as tc LEFT JOIN tbl_calendar_type as tct ON tct.type_id = tc.cal_id";
+
+  await conn.execute(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+      return;
+    }
+    res.status(200).json({
+      message: true,
+      data: result,
+    });
+  });
+});
+
+app.get("/cal_type", async (req, res) => {
+  let sql = "SELECT * FROM tbl_calendar_type";
 
   await conn.execute(sql, (err, result) => {
     if (err) {
